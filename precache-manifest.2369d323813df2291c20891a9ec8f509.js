798339be8dd027260c24b720aafd51bbd60b7c87
Talia Harrison-Marcassa <hm.talia@gmail.com>
2020-03-10 18:18:45 -0400

    Updates

---
+++ precache-manifest.2369d323813df2291c20891a9ec8f509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d01c69c5fa505f752ce7464541f9906",
+    "url": "/flexbox-sporty/index.html"
+  },
+  {
+    "revision": "266fab516781e583bc50",
+    "url": "/flexbox-sporty/static/css/main.6b6c51f7.chunk.css"
+  },
+  {
+    "revision": "b364bda53d699a0d32b6",
+    "url": "/flexbox-sporty/static/js/2.0bcda4e4.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/flexbox-sporty/static/js/2.0bcda4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266fab516781e583bc50",
+    "url": "/flexbox-sporty/static/js/main.09fb50fe.chunk.js"
+  },
+  {
+    "revision": "0dad0e4af89474ad22c8",
+    "url": "/flexbox-sporty/static/js/runtime-main.f479bab8.js"
+  }
+]);
